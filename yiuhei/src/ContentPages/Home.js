4c72f87023bf0088239dd088c2fe960ff5633448
Ash Wang <ash6260@gmail.com>
2020-01-05 01:49:16 -0500

    Added css files for each content page and added content and css for Home page with placeholder image

--- yiuhei/src/ContentPages/Home.js
+++ yiuhei/src/ContentPages/Home.js
@@ -1,9 +1,30 @@
 import React, { Component }  from 'react';
-
+import './Home.css';
 
 class Home extends Component{
   render() {
-    return <>Home</>
+    return (
+      <div id='home-wrapper'>
+        <div id='home-content'>
+          <div id='home-placeholder'/>
+          <div id='home-textbox'>
+            There is always a story.
+
+            <p>There is a story to uncover, and a story waiting to be told.</p>
+
+            <p>Every object embodies a tale, and will fall into place in the one you will unfold.</p>
+
+            <p>Through making we extract a narrative.</p>
+
+            <p>We explore our past to find a need. We imagine our future to envision a solution. We scavenge and assemble in the present in order to bring it to life.</p>
+
+            <p>It&rsquo;s not just about the product, but the journey as well.</p>
+
+            <p>With Love, <br/> Yiu Hei Cheung</p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
