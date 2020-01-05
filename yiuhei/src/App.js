5c859098703df035a8b9d296174a5a75ea6aa07b
Ash Wang <ash6260@gmail.com>
2020-01-04 20:08:19 -0500

    Added basic routes to app.js and blank content pages

--- yiuhei/src/App.js
+++ yiuhei/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {createBrowserHistory} from 'history';
+import HomePage from  './ContentPages/Home';
+import AboutPage from './ContentPages/About';
+import WorkPage from  './ContentPages/Work';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/**
+ * Routes is an array of components accessable by the router.
+ * linkes to the route are displayed on the menu in the top right corner if the onMenu boolean is true.
+ */
+const routes = [
+  {path: '/',       name: 'Home',   onMenu: true, comp: HomePage},
+  {path: '/work',   name: 'Work',   onMenu: true, comp: WorkPage},
+  {path: '/about',  name: 'About',  onMenu: true, comp: AboutPage}
+]
 
+class App extends Component{ 
+  render() {
+    return <BrowserRouter>
+      <>
+        <Link to='/'>Home</Link>
+        <Link to='/work'>Work</Link>
+        <Link to='/about'>About</Link>
+      </>
+      <Route render={({location}) => (
+        <>
+          {routes.map(({path, comp}, id) => (
+            <Route key={path} exact path={path}>
+              {React.createElement(comp, {location:location, history:createBrowserHistory()})}
+            </Route>
+          ))}
+        </>
+      )}/> 
+    </BrowserRouter>
+  }
+}
 export default App;
---
+++ yiuhei/src/ContentPages/Work.js
@@ -0,0 +1,10 @@
+import React, { Component }  from 'react';
+
+
+class Work extends Component{
+  render() {
+    return <>Work</>
+  }
+}
+
+export default Work;
