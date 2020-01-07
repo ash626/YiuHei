34ee9d0369767f3869518135abf9ecb5bead6268
Ash Wang <ash6260@gmail.com>
2020-01-07 02:55:41 -0500

    Added about page styling and content

--- yiuhei/src/ContentPages/About.js
+++ yiuhei/src/ContentPages/About.js
@@ -3,7 +3,22 @@ import './About.css';
 
 class About extends Component{
   render() {
-    return <>About</>
+    return (
+      <div id='about-wrapper'>
+        <div id='about-content'>
+          <div id='whitespace'/>
+          <div id='about-textbox'>
+          <p>I don&rsquo;t really know where to begin.&nbsp;<br />I can tell every story but mine.&nbsp;</p>
+<p><strong>Let&rsquo;s stick to the facts:</strong>&nbsp; I&rsquo;m a graduate from an architecture school with an interest in detailed small scale work and an unhealthy dose of romanticism.&nbsp; All this has led me to pursue stories through design and objects.&nbsp;</p>
+<p><strong>It always begins with questions:</strong> what&rsquo;s the context?&nbsp; Who&rsquo;s involved?&nbsp; What is it made of?&nbsp; How will you interact with it?&nbsp; How will it wear in five, ten, fifteen years?&nbsp;</p>
+<p>With answers to those questions we form a narrative.&nbsp; With narrative, we begin design.&nbsp; At least&hellip; that&rsquo;s how I think it should be.&nbsp;</p>
+<p><strong>The product of all this:</strong> a collection of garments and accessories to clothe characters in.&nbsp;</p>
+<p>I hope you enjoy the adventure as much as I did, and do.</p>
+<p>With Love,<br />Yiu Hei Cheung<br />Toronto, Ontario, Canada&nbsp;</p>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
