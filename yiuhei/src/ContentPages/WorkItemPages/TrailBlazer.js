03e7697e85446b1ec1104903e507406220bea18a
Ash Wang <ash6260@gmail.com>
2020-01-09 01:55:27 -0500

    Added TrailBlazer content

--- yiuhei/src/ContentPages/WorkItemPages/TrailBlazer.js
+++ yiuhei/src/ContentPages/WorkItemPages/TrailBlazer.js
@@ -1,8 +1,149 @@
 import React  from 'react';
+import Grid from '@material-ui/core/Grid';
 
+// Image Imports
+import Img2 from '../../assets/video/trailblazer/02.jpg';
+import Img3 from '../../assets/video/trailblazer/03.jpg';
+import Img4 from '../../assets/video/trailblazer/04.jpg';
+import Img5 from '../../assets/video/trailblazer/05.jpg';
+import Img6 from '../../assets/video/trailblazer/06.jpg';
+import Img7 from '../../assets/video/trailblazer/07.jpg';
+import Img8 from '../../assets/video/trailblazer/08.jpg';
+import Img9 from '../../assets/video/trailblazer/09.jpg';
 const TrailBlazer = () => {
   return (
-    <>TrailBlazer</>
+    <div className="work-page-wrapper">
+    <div className="content">
+    <div class='embed-container'><iframe src='https://player.vimeo.com/video/303411565' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>  
+      <p className='title'>TrailBlazer</p>
+        <span>A Short Film on mediumrare and the Art of Chasing Dreams</span>
+      
+      <div className='divider'/>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>Setting</strong></p>
+          <p>We thought <em>medium rare</em> would be your typical Avant Garde store.&nbsp; It was located in the right area for such a place: in Causeway Bay right next to Times Square.&nbsp; Like another boutique in the area, we had to be buzzed into the gate from the main floor.&nbsp; The building was surprisingly old for a building that was literally beside Times Square.&nbsp; The lift had manually operated doors, the stairways were poorly lit, it seemed like people lived in the building as laundry was hung up along the halls.&nbsp;</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img src={Img2} alt='Setting 1' className='grid-img'/>
+        </Grid>
+
+        <Grid item xs={12}>
+          <p>We peered into the glass door and two men - one older and one younger - smiled and waved us in.  The store felt like a living room with a two-seater couch in the middle facing a large mirror that replaced a household TV.  Two curtain rails that formed a triangle off the mirror plane turned the space before the mirror into a change room.  The clothes sat on metal fixtures that surrounded the couch.  The white window curtains were drawn, blocking out any distractions from the bright LED billboard lights outside.  The lights cast a warm glow onto the plain white walls.  Interesting trinkets (metal skull, some light bulb clock thing, tin wall decorations) littered the space.     </p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Setting 2' src={Img3}/>
+        </Grid>
+      </Grid>
+      
+      <div className='divider'/>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>A Warm Welcome</strong></p>
+          <p>Eddie, the store owner, gladly told us about each piece, the fit, the materials used, the dye processes, the weaving.&nbsp; He would suggest items we might like, who would look better in what garment, whether or not something we picked up would fit us (&ldquo;it doesn&rsquo;t fit, I wouldn&rsquo;t want you to buy it&rdquo;).&nbsp; Felix, his younger assistant and friend, would go about store tasks quietly, interjecting when he felt excited about a given piece.&nbsp;</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='A Warm Welcome 1' src={Img4}/>
+        </Grid>
+        <Grid item xs={12}>
+          <p>We had gotten to a mud-dyed shirt by Yantor.  It must have been special as they eagerly told us about the brand, how their designers would travel to these remote villages to live for months at a time as they learned indigenous methods of production and craft.  They would then shoot their lookbooks with these locals, mixing in their clothing with the clothes of the locale.  “I just thought you would be interested in these stories, I got a feeling you would care, I sensed a vibe,” Eddie said in Cantonese.  He then repeated it in English to Annie.  He took the care to translate and repeat for us.  </p>
+        </Grid>
+      </Grid>
+
+      <div className='divider'/>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>Eddie&rsquo;s Past</strong></p>
+          <p>The living room setting was extremely fitting for this encounter, as Eddie ran with his &ldquo;vibe&rdquo; and shared with us his stories. We got cozy by the couch and listened with great interest, only speaking when necessary.</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Eddies Past 1' src={Img5}/>
+        </Grid>
+        <Grid item xs={12}>
+          <p>Eddie&rsquo;s family moved to San Francisco from Hong Kong in his youth.&nbsp; He grew up on the streets, witnessed and possibly partook in gang activity. &nbsp;&ldquo;I&rsquo;ve seen dead people, lying on the streets.&nbsp; I&rsquo;ve had a gun held to my head.&nbsp; I&rsquo;ve been arrested.&rdquo; &nbsp;He had valued experience over education and began working when he was 14.&nbsp; As such, he never properly learned to read and write.&nbsp;</p>
+          <p>Eddie&rsquo;s first passion was music.&nbsp; He used to sing while he bussed tables.&nbsp; One day, he got hit by a truck and could no longer bus tables, so his friend suggested that he enter Cantonese pop singing contests to win prizes and sell them for profit since he sung so well.&nbsp; He never listened to Cantonese pop before but upon hearing it he realized it was a lot easier to sing than Joy Division, so he entered contests around the city.&nbsp; He couldn&rsquo;t compete with the top singers so he learned to dance so he could nab second place in vocals and first place in dancing, collecting two prizes instead of one.&nbsp; He had won cars, TVs, flights back to Hong Kong. &nbsp;</p>
+          <p>He used to buy Levi&rsquo;s 501s in The States and bring them back to Hong Kong during his visits in order to sell them through his friend&rsquo;s consignment stores.&nbsp; They were a huge hit, so the friend suggested he start selling vintage clothing. &nbsp;</p>
+        </Grid>
+      </Grid>
+
+      <div className='divider'/>
+
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>medium rare</strong></p>
+          <p>Eddie told us about his business ventures, his struggles, his conflicts.&nbsp; He&rsquo;s been doing this for over two decades now.&nbsp; His staff, store, location, and stock has changed drastically through the times.&nbsp;</p>
+          <p>He shared stories of how a rich customer demanded that Eddie sold him his personal leather jacket (which he had lovingly worn-in for 13 years to what he thought was perfection), or else he wouldn&rsquo;t buy the 9 other jackets he was interested in.&nbsp;</p>
+          <p>He shared about the difficulties being poor and without fame in this industry, how brands would ridicule him for his small store space and lack of money as bigger retailers grew popular in the city.&nbsp;</p>
+          <p>&ldquo;An old Chinese lady came into the store one day and asked me how to sign brands.&nbsp; Her daughter owned a store in China and she can&rsquo;t compete with TaoBao knockoffs and pricing.&nbsp; I told her &lsquo;you&rsquo;ve got it all wrong, I can&rsquo;t help you.&nbsp; If you wanted to know how I stay so slim, why I&rsquo;m so lonely, I could tell you.&nbsp; I can only afford to eat one meal a day, that&rsquo;s why I&rsquo;m so slim.&nbsp; I can&rsquo;t afford to have a wife or child, that&rsquo;s why I&rsquo;m so lonely.&nbsp; That&rsquo;s the only way I can sign these brands and run this store.&rdquo;&nbsp;</p>
+          <p>He shared stories of betrayal from people he thought were friends, people he considered business partners.&nbsp; He shared stories of how his old staff had stabbed his back, stole his product, and started up their own stores.&nbsp; He took the blame for it, telling us about how he couldn&rsquo;t provide for his staff, he couldn&rsquo;t provide them the growth and development they desired.</p>
+          <p>&ldquo;I eat one meal a day so I can wear the clothes I wear, so I can own a shop. They said, &lsquo;Eddie, we don&rsquo;t want to live like this, we can&rsquo;t live like this.&rsquo; They were right, I had to give them something.&rdquo;</p>
+          <p>But through all these negative tales he had a sense of awareness and positivity, acknowledging his own faults and embracing the joy of shedding those that were toxic in his life.&nbsp; He was thankful that he got to sign the brands he has now.&nbsp; Thankful for the opportunity to support people he sees value in. &nbsp;&ldquo;I&rsquo;m not that big, I am not able to launch someone into success, but I want to help because I believe in them.&rdquo; &nbsp;</p>
+          <p>Eddie was aware that trends tend to ruin fashion.&nbsp; That the rich and thoughtless will drive a trend to death and ruin a label, and in turn, clothes will be thrown out and the oceans will be littered.&nbsp; He was also aware that a portion of his customers would be those who cared not for the craft behind each product but the status and style they could achieve if they bought them. &nbsp;To those customers, Eddie&rsquo;s thoughts, opinions, and values would remain hidden. &ldquo;I don&rsquo;t share much with those customers, not like I&rsquo;m sharing with you.&nbsp; I see value in keeping some things secret.&nbsp; If you can understand that then you can understand the joke in our store name.&rdquo;&nbsp;&nbsp;</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Medium Rare 1' src={Img6}/>
+        </Grid>
+        <Grid item xs={12}>
+          <p>He now runs the store with Felix, his friend and assistant. &nbsp;&ldquo;We get to talk about our interests, the past and the future.&nbsp; We spend most of our days talking shit.&rdquo; &nbsp;When they weren&rsquo;t talking to us they kept up a light banter.&nbsp; Knowing that we were foreigners on vacation, Felix had made a few suggestions for food and places to visit. &nbsp;&nbsp;</p>
+          <p>&ldquo;You must do something that requires a lot of feeling...&rdquo; Eddie said when I asked him what he thought Annie did for a living.&nbsp; It&rsquo;s that level of intuition that caught my interest.&nbsp; How he could pick up on a vibe and run with it. How he appreciated those who appreciated detail. &nbsp;</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Medium Rare 2' src={Img7}/>
+        </Grid>
+      </Grid>
+
+      <div className='divider'/>
+
+
+      <div className='divider'/>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>Chasing a Dream</strong></p>
+          <p>I grew up in a household that valued security above all. When I asked him what his dreams were, my father would say &ldquo;I have none.&rdquo; I had told him I didn&rsquo;t want to work in Architecture anymore, that I wanted to walk my own path. He said, &ldquo;when you fail, just know you can always go back.&rdquo;</p>
+          <p>I don&rsquo;t hold it against him. It&rsquo;s only natural for a first generation immigrant parent to place the stability of their child above all. It&rsquo;s only natural for it to pain them when their child is choosing to leave the comfort of safety in order to enter the unknown. Though this emphasis on stability had left an imprint on me.</p>
+          <p>I am afraid to reach my potential.</p>
+          <p>I know what I want to be, or rather I know what I don&rsquo;t want to be. I don&rsquo;t want to feel trapped in what I do. I don&rsquo;t want to see my talents go to waste. I don&rsquo;t want my interests to fall into insignificance.</p>
+          <p>But I am afraid to live the life I want to live. I am afraid to leave the stability of a safe job. I am afraid to risk my time and finances to learn new skills. I am afraid I won&rsquo;t impress, afraid that I am not good enough. I am afraid that I will end up with nothing in my search for everything. I am afraid to leave the comfort that these fears bring me.</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Chasing a Dream 1' src={Img8}/>
+        </Grid>
+        <Grid item xs={12}>
+          <p>Eddie had given me a lot to learn from. There I was, sitting in what essentially was his living room, listening to him share a life of constant struggle and betrayal. He had lived through all of that, yet he remained kind, humble, and self-aware. He had lived through all of that, but he is happy because he is doing what he needs to be doing.</p>
+          <p>Eddie is constantly facing pressure from larger retailers that wish to strong-arm him into giving up his accounts. &ldquo;You&rsquo;re small, Eddie, and poor. Just give it up, we can sell more units, they&rsquo;ll be happier.&rdquo; He would tell them, &ldquo;You may have money, but I have nothing. I will continue to do this, so don&rsquo;t fight me. I can do this because I am crazier than you.&rdquo;</p>
+        </Grid>
+        <Grid item xs={12}>
+          <img class='grid-img' alt='Chasing a Dream 2' src={Img9}/>
+        </Grid>
+        <Grid item xs={12}>
+          <p>It was through these stories that I realized what it was I lacked. Eddie grew up with nothing. He&rsquo;s always had nothing. He is comfortable with nothing. With nothing, you have no worries. With nothing, you only know how to follow that gut feeling, the feeling that brings you closer to your dreams.</p>
+          <p>So maybe what I need is to be comfortable with the idea of &ldquo;nothing&rdquo;. What I need is to not be afraid of losing something on my path to everything.</p>
+        </Grid>
+      </Grid>
+
+      <div className='divider'/>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <p><strong>On the Film</strong></p>
+          <p>I had composed the film using candid footage taken after Eddie had shared what he needed to share. It felt wrong to step into someone&rsquo;s space and start recording before I even knew them. This definitely would not have come about if he had not begun our dialogue, so I am thankful that he did.</p>
+          <p>I am thankful for the opportunity to hear what was said, and I hope the story can inspire someone the way it inspired me.</p>
+        </Grid>
+
+      </Grid>
+
+      <div className='divider'/>
+
+      <div className='credits'>
+        <p><strong>Credits</strong></p>
+        <p>Videography, Editing, and Narrative by Yiu Hei Cheung<br />B-Roll Footage by Annie Wang<br />Special Thanks to Eddie &amp; Felix of mediumrare<br />December 2018</p>
+      </div>
+
+
+    </div>
+  </div>
   )
 };
 
