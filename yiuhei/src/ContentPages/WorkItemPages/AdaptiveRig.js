3eaf4f10883b10f4df4093bdab8048aba4a1b19f
Ash Wang <ash6260@gmail.com>
2020-01-09 00:12:25 -0500

    Added Adaptive rig content page

--- yiuhei/src/ContentPages/WorkItemPages/AdaptiveRig.js
+++ yiuhei/src/ContentPages/WorkItemPages/AdaptiveRig.js
@@ -1,8 +1,126 @@
 import React  from 'react';
+import Grid from '@material-ui/core/Grid';
+import AwesomeSlider from 'react-awesome-slider';
+
+// Image imports
+import Banner from '../../assets/clothing/adaptive_rig/banner.jpg';
+import Img2 from '../../assets/clothing/adaptive_rig/02.jpg';
+import Img3 from '../../assets/clothing/adaptive_rig/03.jpg';
+import Img4 from '../../assets/clothing/adaptive_rig/04.jpg';
+import Img5 from '../../assets/clothing/adaptive_rig/05.jpg';
+import Img6 from '../../assets/clothing/adaptive_rig/06.jpg';
+import Img7 from '../../assets/clothing/adaptive_rig/07.jpg';
+import Img8 from '../../assets/clothing/adaptive_rig/08.gif';
+import Img9 from '../../assets/clothing/adaptive_rig/09.jpg';
+import Img10 from '../../assets/clothing/adaptive_rig/10.jpg';
+import Img11 from '../../assets/clothing/adaptive_rig/11.jpg';
+import Img12 from '../../assets/clothing/adaptive_rig/12.jpg';
+import Img13 from '../../assets/clothing/adaptive_rig/13.jpg';
+import Img14 from '../../assets/clothing/adaptive_rig/14.jpg';
+import Img15 from '../../assets/clothing/adaptive_rig/15.jpg';
+import Img16 from '../../assets/clothing/adaptive_rig/16.jpg';
 
 const AdaptiveRig = () => {
   return (
-    <>AdaptiveRig</>
+    <div className="work-page-wrapper">
+      <div className="content">
+        <img className='banner' alt='Banner' src={Banner}/>
+          <p className='title'>Adaptive Rig</p>
+          <span>A totem of resilience.</span>
+        
+        <div className='divider'/>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <p><strong>Summer of Discontent</strong></p>
+            <p>Since early June of 2019, millions of Hong Kong citizens have been filling the streets in protest against a proposed extradition bill and the accelerated loss of their semi-autonomous government and freedoms.&nbsp;&nbsp;</p>
+            <p>Inevitable clashes with the Hong Kong Police and anti-protest parties have led to acts of violence tainting the once peaceful protests.&nbsp; As tensions rise, tear gas canisters, batons, rubber bullets, metal rods, graffiti, bricks and stones become common in reports surrounding the protests.&nbsp; With footage of military vehicles moving towards Hong Kong trickling through international media and pro-democracy demonstrations happening in foreign cities, it is apparent that the fight for democratic freedom is not stopping any time soon.&nbsp;</p>
+            <p>While it&rsquo;s naive to believe that revolution can come without violence, the violence is still concerning.&nbsp; With the majority of my family in Hong Kong, with friends attending the protests and relatives on the police force, I hope for the safety of all those involved.&nbsp; I hope that those who are pushing for freedom can achieve it through peace. I hope that the government can find a resolution without infringing on the safety of the people.</p>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='Summer of Discontent' src={Img2}/>
+          </Grid>
+        </Grid>
+        
+        <div className='divider'/>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <p><strong>A Transformed People</strong></p>
+            <p>It&rsquo;s inspiring, to say the least, to see almost a third of the city drop their obligations and responsibilities to unify and stand for the future of their home.&nbsp;</p>
+            <p>Over the course of three months, students, lawyers, professors, everyday people shed their status as mere bystanders, put themselves in the face of danger and criticism in order to become the face of democracy.&nbsp;&nbsp;</p>
+            <p>It&rsquo;s sad to see what started with peace be met with violence, which in turn led to violence perpetrated by both sides.&nbsp; It&rsquo;s sad to see batons and boots collide with bodies, teargas and water cannons deployed in populated areas. It&rsquo;s sad to see certain protesters enact violence and bring a bad name to the cause.&nbsp;&nbsp;</p>
+            <p>I hope that those who remained peaceful will continue to do so.&nbsp; I hope that they will push on for the sake of a better future. I hope that they stay resilient and strong in the face of violence and danger, knowing that their safety lies in unrelenting peace and will.&nbsp;</p>          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='A Transformed People 1' src={Img3}/>
+          </Grid>
+          <Grid item xs={12}>
+            <img className="grid-img" alt='A Transformed People 2' src={Img4}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='A Transformed People 3' src={Img5}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='A Transformed People 4' src={Img6}/>
+          </Grid>
+          <Grid item xs={12}>
+            <img className="grid-img" alt='A Transformed People 5' src={Img7}/>
+          </Grid>
+        </Grid>
+
+        <div className='divider'/>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <p><strong>The Rig</strong></p>
+            <p>A HKU Tote Bag was repurposed into a transforming satchel.&nbsp; The tote was cut up so that the plain black fabric would compose the base bag.&nbsp; A secondary pouch was made from the screen printed fabric. The pouch attaches onto the PALS webbing grid sewn onto the bag.&nbsp;&nbsp;</p>
+            <p>With the lateral straps tucked behind, the bag can be worn as a satchel, operating as a discreet everyday carry sling for an everyday person.&nbsp;&nbsp;</p>
+            <p>A spare nylon strap stored in the secondary pouch can be attached to the lateral straps, the main strap can be shortened, and the bag can be secured centered on the chest, like a military plate carrier.&nbsp; In practice, a plate carrier holds armour plates that provide soldiers protection, a symbol of strength and resilience.&nbsp;&nbsp;</p>
+            <p>D-rings sewn into the straps offer additional attachment points for accessories.&nbsp;</p>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 1' src={Img8}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 2' src={Img9}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 3' src={Img10}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 4' src={Img11}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 5' src={Img12}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 6' src={Img13}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 7' src={Img14}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 8' src={Img15}/>
+          </Grid>
+          <Grid item xs={6}>
+            <img className="grid-img" alt='The Rig 9' src={Img16}/>
+          </Grid>
+        </Grid>
+
+        <div className='divider'/>
+
+        <div className='credits'>
+          <p><strong>Credits</strong></p>
+          <p>Bag, Narrative, Studio Photography and Collages by Yiu Hei Cheung<br />On-site Photography by Kyle Ortuouste<br />August 2019&nbsp;</p>
+          <p><strong>Details</strong></p>
+          <p>Body: 100% Upcycled Cotton<br />Straps: 100% Nylon</p>
+        </div>
+
+
+      </div>
+    </div>
+
+
   )
 };
 
